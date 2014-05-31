4ba1b1e5c8660e3ce5e109a9e1bf6f400a8891d0
Travis <travis@travis-ci.org>
2014-05-31 15:26:15 +0000

    Update docs from Travis build 60

---
+++ doc/implementors/core/fmt/trait.Show.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['gnuplot'] = {Coordinate:"gnuplot/coordinates/type.Coordinate.html",};
+
+                if (window.register_implementors) {
+                    window.register_implementors(implementors);
+                } else {
+                    window.pending_implementors = implementors;
+                }
+            
+})()
