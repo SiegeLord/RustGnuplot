b01c8185cf33292ddc9afa20ce789de1a6aab7f2
Travis <travis@travis-ci.org>
2014-06-02 11:13:41 +0000

    Update docs from Travis build 60

---
+++ doc/implementors/gnuplot/trait.AxesCommon.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['gnuplot'] = ["impl <a class='trait' href='gnuplot/trait.AxesCommon.html' title='gnuplot::AxesCommon'>AxesCommon</a> for <a class='struct' href='gnuplot/struct.Axes2D.html' title='gnuplot::Axes2D'>Axes2D</a>","impl <a class='trait' href='gnuplot/trait.AxesCommon.html' title='gnuplot::AxesCommon'>AxesCommon</a> for <a class='struct' href='gnuplot/struct.Axes3D.html' title='gnuplot::Axes3D'>Axes3D</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
