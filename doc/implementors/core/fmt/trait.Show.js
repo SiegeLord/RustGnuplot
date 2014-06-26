e8aada22b31e394f7bdf751c3d6df27c1cfe1d4a
Travis <travis@travis-ci.org>
2014-06-26 11:23:40 +0000

    Update docs from Travis build 62

---
+++ doc/implementors/core/fmt/trait.Show.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['gnuplot'] = ["impl <a class='trait' href='http://doc.rust-lang.org/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='type' href='gnuplot/coordinates/type.Coordinate.html' title='gnuplot::coordinates::Coordinate'>Coordinate</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
