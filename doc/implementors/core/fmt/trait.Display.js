3ffe3828d5b901bcb6a879eecbcd17e9dc7441d7
Travis <travis@travis-ci.org>
2015-01-24 19:03:22 +0000

    Update docs from Travis build 99

---
+++ doc/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['gnuplot'] = ["<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='gnuplot/enum.Coordinate.html' title='gnuplot::Coordinate'>Coordinate</a>","<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='gnuplot/enum.Coordinate.html' title='gnuplot::Coordinate'>Coordinate</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
