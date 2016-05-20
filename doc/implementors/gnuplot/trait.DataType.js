a06a47efc09ebbc4105e9e4543a0fa5a4859f777
Travis <travis@travis-ci.org>
2016-05-20 21:44:35 +0000

    Update docs from Travis build 270

--- doc/implementors/gnuplot/trait.DataType.js
+++ doc/implementors/gnuplot/trait.DataType.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['gnuplot'] = [];
+implementors["gnuplot"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
