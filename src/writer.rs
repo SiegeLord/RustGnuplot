389d39b1bcb810cce700b674e087d19c11a9b5af
Caleb Whiting <caleb@whiting.io>
2019-04-15 10:08:02 -0400

    fix formatting to match existing style

--- src/writer.rs
+++ src/writer.rs
@@ -5,12 +5,15 @@
 use byteorder::{LittleEndian, WriteBytesExt};
 use std::io::{self, Write};
 
-pub trait Writer: Write {
-	fn write_str(&mut self, s: &str) -> Result<(), io::Error> {
+pub trait Writer: Write
+{
+	fn write_str(&mut self, s: &str) -> Result<(), io::Error>
+	{
 		self.write_all(s.as_bytes())
 	}
 
-	fn write_le_f64(&mut self, v: f64) -> Result<(), io::Error> {
+	fn write_le_f64(&mut self, v: f64) -> Result<(), io::Error>
+	{
 		self.write_f64::<LittleEndian>(v)
 	}
 }
