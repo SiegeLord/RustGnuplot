b7615d0edc37caf37b027da2cac215e5a1de5b9d
Caleb Whiting <caleb@whiting.io>
2019-04-15 10:01:19 -0400

    use byteorder crate for writing f64 little endian

--- src/writer.rs
+++ src/writer.rs
@@ -2,23 +2,16 @@
 //
 // All rights reserved. Distributed under LGPL 3.0. For full terms see the file LICENSE.
 
+use byteorder::{LittleEndian, WriteBytesExt};
 use std::io::{self, Write};
-use std::mem::transmute;
 
-pub trait Writer: Write
-{
-	fn write_str(&mut self, s: &str) -> Result<(), io::Error>
-	{
+pub trait Writer: Write {
+	fn write_str(&mut self, s: &str) -> Result<(), io::Error> {
 		self.write_all(s.as_bytes())
 	}
 
-	fn write_le_f64(&mut self, v: f64) -> Result<(), io::Error>
-	{
-		let vb: [u8; 8] = unsafe {
-			let v: u64 = transmute(v);
-			transmute(v.to_le())
-		};
-		self.write_all(&vb)
+	fn write_le_f64(&mut self, v: f64) -> Result<(), io::Error> {
+		self.write_f64::<LittleEndian>(v)
 	}
 }
 
