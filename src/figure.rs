86e80375da03afa7fab3b6ec80a070d5e8c0db5e
Phileas Fogg <phileas.fogg@mail.com>
2019-05-24 21:34:50 +0200

    Add save_to_png function
    An easy figure's function to save as png file.
    Tested functional
    Users do not need to know about terminals, pngcairo or wxt...

--- src/figure.rs
+++ src/figure.rs
@@ -230,6 +230,16 @@ impl Figure
 		self
 	}
 
+	  /// Save to a file the the commands that if piped to a gnuplot process would display the figure
+	  /// # Arguments
+	  /// * `file_path` - Path to the output file (png)
+    pub fn save_to_png(&mut self, file_path: &str)
+    {
+	      self.set_terminal("pngcairo", file_path);
+	      self.show().close();
+        self.set_terminal("wxt", &"");
+    }
+
 	/// Closes the gnuplot process.
 	///
 	/// This can be useful if you're your plot output is a file and you need to
