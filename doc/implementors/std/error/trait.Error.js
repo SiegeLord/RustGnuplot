8aa468f6df12942d795b815da136d44f98f6a17a
Travis <travis@travis-ci.org>
2019-12-14 19:07:56 +0000

    Update docs from Travis build 1630

--- doc/implementors/std/error/trait.Error.js
+++ doc/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["gnuplot"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"gnuplot/struct.GnuplotInitError.html\" title=\"struct gnuplot::GnuplotInitError\">GnuplotInitError</a>",synthetic:false,types:["gnuplot::error_types::GnuplotInitError"]},];
+implementors["gnuplot"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"gnuplot/struct.GnuplotInitError.html\" title=\"struct gnuplot::GnuplotInitError\">GnuplotInitError</a>","synthetic":false,"types":["gnuplot::error_types::GnuplotInitError"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
