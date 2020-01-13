482651d5ebd392acba06be64fdd0d1d12a3b9ad9
Travis <travis@travis-ci.org>
2020-01-13 03:34:46 +0000

    Update docs from Travis build 1662

---
+++ doc/implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["gnuplot"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"gnuplot/struct.Figure.html\" title=\"struct gnuplot::Figure\">Figure</a>","synthetic":false,"types":["gnuplot::figure::Figure"]}];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
