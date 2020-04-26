91f8c4b6607cb785c3ceefe2e6b41e780c7022a9
Travis <travis@travis-ci.org>
2020-04-26 08:25:11 +0000

    Update docs from Travis build 1774

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,5 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["byteorder"] = {"name":"","files":["io.rs","lib.rs"]};
-sourcesIndex["delete_me"] = {"name":"","files":["dummy.rs"]};
 sourcesIndex["gnuplot"] = {"name":"","files":["axes2d.rs","axes3d.rs","axes_common.rs","coordinates.rs","datatype.rs","error_types.rs","figure.rs","lib.rs","options.rs","util.rs","writer.rs"]};
 createSourceSidebar();
