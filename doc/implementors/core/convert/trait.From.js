48017d22dfe8d29564b6321ab002e28d5bc3df37
Travis <travis@travis-ci.org>
2021-01-28 11:41:47 +0000

    Update docs from Travis build 2066

--- doc/implementors/core/convert/trait.From.js
+++ doc/implementors/core/convert/trait.From.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["gnuplot"] = [{"text":"impl From&lt;Error&gt; for GnuplotInitError","synthetic":false,"types":[]}];
+implementors["gnuplot"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/std/io/error/struct.Error.html\" title=\"struct std::io::error::Error\">Error</a>&gt; for <a class=\"struct\" href=\"gnuplot/struct.GnuplotInitError.html\" title=\"struct gnuplot::GnuplotInitError\">GnuplotInitError</a>","synthetic":false,"types":["gnuplot::error_types::GnuplotInitError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
