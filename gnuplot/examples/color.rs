ff99e959fa54a69edafe4c44367be3668da29a5e
Ed Rogers <etfrogers@hotmail.com>
2025-02-23 13:07:45 +0000

    Finish first color example and put in a loop

--- gnuplot/examples/color.rs
+++ gnuplot/examples/color.rs
@@ -29,10 +29,10 @@ fn example(c: Common) {
 		Color("black"),
 		Color(ColorType::RGBColor("black")),
 		Color("red"),
-		Color(ColorType::RGBColor("#ff0000")),   // red using Hex coded RRGGBB
-		Color(ColorType::RGBColor("#ff8888")),   // pink using Hex coded RRGGBB
+		Color(ColorType::RGBColor("#ff0000")), // red using Hex coded RRGGBB
+		Color(ColorType::RGBColor("#ff8888")), // pink using Hex coded RRGGBB
 		Color(ColorType::RGBColor("#88ff0000")), // pink using Hex coded AARRGGBB
-		Color(ColorType::RGBColor("#ff0000")),   // red using Hex coded RRGGBB
+		Color(ColorType::RGBColor("#ff0000")), // red using Hex coded RRGGBB
 	];
 
 	for (i, color) in colors.into_iter().enumerate() {
@@ -45,60 +45,83 @@ fn example(c: Common) {
 
 	c.show(&mut fg, "rgb_color");
 
-
-	let data:Vec<Vec<f64>> = CANDLESTICKS_STR.split("\n").map(|line| line.split("\t").map(|v| v.trim().parse::<f64>().unwrap()).collect()).collect();
+	let data: Vec<Vec<f64>> = CANDLESTICKS_STR
+		.split("\n")
+		.map(|line| {
+			line.split("\t")
+				.map(|v| v.trim().parse::<f64>().unwrap())
+				.collect()
+		})
+		.collect();
 	let extract_col = |i| data.iter().map(|l| l[i]).collect::<Vec<_>>();
 
 	let d1 = extract_col(0);
 	let d2 = extract_col(1);
-	let d3 = extract_col(2);
+	// let d3 = extract_col(2);
 	// let d4 = extract_col(3);
 	let d5 = extract_col(4);
 	let d6 = extract_col(5);
-	let row_index:Vec<_>  = (1..=d1.len() as u8).collect();
+	let row_index: Vec<_> = (1..=d1.len() as u8).collect();
+	let by3 = |x| (((x % 3.0) + 1.0) / 6.0);
+	let by4 = |x| (((x % 4.0) + 1.0) / 7.0);
 
 	// Demo/test of variable color in many different plot styles
-	// derived from plot 1 in https://gnuplot.sourceforge.net/demo_6.0/varcolor.html
-	// this demonstrates usage of VariableIndexColor with indcies to use gnuplots dfefalt color styles,
-	// but make them align for multiple plot items on the same axis.
-	// i.e. evertything at x = 1 uses the first gnuplot color, everything at x = 2 uses the second and so on.
-	let mut fg = Figure::new();
-	let ax = fg.axes2d();
-	ax.set_title("variable color points, candlesticks, boxes, and boxxyerror", &[]);
-	ax.set_y_range(Fix(-4.0), Fix(10.0));
-	ax.set_x_range(Fix(0.0), Fix(11.0));
+	// derived from https://gnuplot.sourceforge.net/demo_6.0/varcolor.html
+	//
+	// The loop is run four times with different color sets: each one sets all the elements of a given
+	// plot to a different color, while making sure the colors align by x position: i.e. everything at x = 1
+	// uses the first color, everything at x = 2 uses the second and so on.
+	//
+	//
+	// The first color loop demonstrates usage of VariableIndexColor with indices to use gnuplot's default color styles,
+	// but make them align for multiple plot items on the same axis. This is implicity constructed from a Vec<u8> using
+	// the `Color` function but could equivalently be created explicitly using `ColorType::VariableIndexColor(row_index.clone())`
+	//
+	// The second color loop ...
+	for color in [Color(row_index.clone())] {
+		let mut fg = Figure::new();
+		let ax = fg.axes2d();
+		ax.set_title(
+			"variable color boxerror, xyerrorbars, impulses, vectors, and labels",
+			&[],
+		);
+		ax.set_y_range(Fix(-4.0), Fix(11.5));
+		// unset colorbox
+		ax.box_error_low_high_set_width(
+			&d1,
+			&d5,
+			&d2,
+			&d6,
+			iter::repeat(0.2),
+			&[
+				color.clone(),
+				FillAlpha(0.5),
+				BorderColor(RGBColor("black")),
+			],
+		);
+		ax.points(&d1, iter::repeat(1), &[color.clone(), PointSymbol('D')]);
+		ax.xy_error_bars(
+			&d1,
+			iter::repeat(8),
+			d1.iter().map(by3),
+			d1.iter().map(by4),
+			&[color.clone()],
+		);
+		ax.points(
+			&d1,
+			d2.iter().map(|v| -v / 2.0),
+			&[color.clone(), PointSymbol('O'), PointSize(3.0)],
+		);
+		ax.box_xy_error_delta(
+			&d1,
+			iter::repeat(10),
+			d1.iter().map(by3),
+			d1.iter().map(by4),
+			&[color.clone()],
+		);
 
-	let by3 = |x| (((x%3.0)+1.0)/6.0);
-	let by4 = |x| (((x%4.0)+1.0)/7.0);
-
-	// let ax.financebars(...)
-	// points replaces circles (as circles not implemented)
-	ax.points(&d1, &d2, &[Color(VariableIndexColor(row_index.clone())), PointSymbol('O'), PointSize(3.0)]);
-	ax.box_xy_error_delta(
-		&d1,
-		iter::repeat(8),
-		d1.iter().map(by3),
-		d1.iter().map(by4),
-		&[Color(VariableIndexColor(row_index.clone()))]);
-	ax.boxes_set_width(
-		&d1,
-		d2.iter().map(|v| -v/2.0),
-		iter::repeat(0.2),
-		&[Color(VariableIndexColor(row_index.clone()))]);
-	// box_and_whisker is rust gnuplot's name for candlestick
-	ax.box_and_whisker_set_width(
-		&d1,
-		d3,
-		d2,
-		d6,
-		d5,
-		iter::repeat(0.3),
-		&[Color(VariableIndexColor(row_index.clone()))]);
-	// set boxwidth 0.2 abs
-	// set bars front
-	// rgbfudge(x) = x*51*32768 + (11-x)*51*128 + int(abs(5.5-x)*510/9.)
-	c.show(&mut fg, "variable_color");
-
+		c.show(&mut fg, "variable_color");
+	}
 }
 
 fn main() {
